48b46385d0d3536308d19164f96dd43b8de661c2
mukesh sriram <srirammukesh@gmail.com>
2020-05-19 00:33:40 -0500

    RightAngleTriangle pattern

---
+++ Nested4Loops/RightAngleTriangle.java
@@ -0,0 +1,13 @@
+class RightAngleTriangle{
+public static void main(String args[]){
+for(int i =1;i<=5;i++){
+for(int x =5;x>=i;x--){
+System.out.print(" ");
+}
+for(int j =1;j<=i;j++){
+System.out.print(j+" ");
+}
+System.out.println();
+}
+}
+}
